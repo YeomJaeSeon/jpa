07043fc56481bc4be18cf4a602a2953f94781625
YeomJaeSeon <a89541457@gmail.com>
2021-07-07 14:06:39 +0900

    엔티티 클래스 개발

---
+++ jpa활용1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDER_ITEM")
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    private int orderPrice; // 주문가격
+
+    private int count; // 주문수량
+}
